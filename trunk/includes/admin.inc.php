03c5cdb17cf278d89bd5105d88fe05be2756d6e6
antoine.precigout@gmail.com <antoine.precigout@gmail.com@6b852ef6-e093-1255-c8e3-cc572f8de254>
2011-01-20 17:23:27 +0000

    Structure SQL et includes de base (admin singleton)

---
+++ trunk/includes/admin.inc.php
@@ -0,0 +1,44 @@
+<?php
+include('.\config.php');
+class Admin
+{
+	private $bdd;
+	private static $unique=NULL;
+	
+	private function __construct()
+    {
+    	try
+		{
+			$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+			$this->bdd = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_DBNAME, $DB_USERNAME, $DB_PASSWORD, $pdo_options);
+		}
+		catch (Exception $e)
+		{
+				die('Erreur : ' . $e->getMessage());
+		}
+    	Admin::unique = $this;
+    }
+	
+	private function __destruct()
+    {
+    }
+	
+	public static function getInstance()
+	{
+		if($unique!=NULL)
+		{
+			return Admin::unique;
+		}
+		else
+		{
+			Admin::__constuct();
+			return Admin::unique;
+		}
+	}
+	
+	public nouveau_stage($nom, $obj, $prog, $intervenant, $prerequis, $duree, $lieu, $nivcomp, $theme)
+	{
+		//Pas fini
+		$this->bdd->exec('INSERT INTO formation(nom, possesseur, console, prix, nbre_joueurs_max, commentaires) VALUES ('',$nom, $obj, $prog, $intervenant, $prerequis, $duree, $lieu, $nivcomp, $theme);
+	}
+}
