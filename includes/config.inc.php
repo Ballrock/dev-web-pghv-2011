5116d9f8ab017d683942877289cee181d671b8a3
antoine.precigout@gmail.com <antoine.precigout@gmail.com@6b852ef6-e093-1255-c8e3-cc572f8de254>
2011-01-20 17:23:27 +0000

    Structure SQL et includes de base (admin singleton)

---
+++ includes/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+/*------- /includes/config.inc.php ---------
+
+Gestion de Catalogue de formation
+
+Date :
+Auteurs :
+
+------------------------------------------*/
+
+$DB_HOST='localhost';
+$DB_DBNAME='catalogue_formation';
+$DB_USERNAME='root';
+$DB_PASSWORD='';
+
+?>
