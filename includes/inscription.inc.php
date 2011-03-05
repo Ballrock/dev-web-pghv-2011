8c217a1c0f97b20ce710a5dd5d681ed3515ed7b0
antoine.precigout@gmail.com <antoine.precigout@gmail.com@6b852ef6-e093-1255-c8e3-cc572f8de254>
2011-03-05 18:08:06 +0000

    Inscription des Utilisateur de niveau 1 fini

---
+++ includes/inscription.inc.php
@@ -0,0 +1,113 @@
+<?php
+class Inscription
+{
+	private $bdd;
+	private $nom;
+	private $prenom;
+	private $status;
+	private $email;
+	private $mdp;
+	
+	public function __construct()
+	{
+		if(!isset($DB_DBNAME))
+		{
+			include('includes\config.inc.php');
+		}
+		try
+		{
+			$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+			$this->bdd = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_DBNAME, $DB_USERNAME, $DB_PASSWORD, $pdo_options);
+		}
+		catch (Exception $e)
+		{
+				die('Erreur : ' . $e->getMessage());
+		}
+	}
+	
+	public function __destruct()
+	{
+	}
+	
+	public function register_values($nom,$prenom,$status,$email,$mdp)
+	{
+		$error="noerror";
+		if($nom=="" || $prenom=="" || $status=="" || $email=="" || $mdp=="")
+		{
+			$error="Un des champs de l'inscription est vide";
+		}
+		else
+		{
+			if(!is_numeric($status))
+			{
+				$error="Probleme avec le status ";
+			}
+			else
+			{
+				if(!filter_var($email, FILTER_VALIDATE_EMAIL)){ 
+					$error="Mauvais format pour l'émail";
+				}
+				else
+				{
+					try
+					{
+						$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+						$req = $this->bdd->prepare('SELECT ID_UTILISATEUR FROM utilisateur WHERE EMAIL=:email', $pdo_options);
+						$req->execute(array(
+							'email' => $email,
+							));
+						while ($donnees = $req->fetch())
+						{
+							$test = $donnees['ID_UTILISATEUR'];
+						}		
+					}
+					catch (Exception $e)
+					{
+						return $e->getMessage();
+					}
+					if (isset($test))
+					{
+						$error="Email déjà éxistant";
+					}
+					else
+					{
+							$nom = htmlentities($nom);
+							$prenom = htmlentities($prenom);
+							$email = htmlentities($email);
+							$mdp = htmlentities($mdp);
+						
+							if (get_magic_quotes_gpc()) 
+							{
+								$nom = stripslashes($nom);
+								$prenom = stripslashes($prenom);
+								$email = stripslashes($email);
+								$mdp = stripslashes($mdp);
+							}
+							$nom = mysql_real_escape_string($nom);
+							$prenom = mysql_real_escape_string($prenom);
+							$email = mysql_real_escape_string($email);
+							$mdp = mysql_real_escape_string($mdp);
+							try
+							{
+								$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+								$req = $this->bdd->prepare('INSERT INTO utilisateur(ID_UTILISATEUR, NOM, PRENOM, EMAIL, MOTDEPASSE, STATUS, DATEINSCRIPT, NIVEAU) VALUES ("", :nom, :prenom, :email, :motdepasse, :status, :date, 1)', $pdo_options);
+								$req->execute(array(
+									'nom' => $nom,
+									'prenom' => $prenom,
+									'email' => $email,
+									'motdepasse' => sha1($mdp),
+									'status' => $status,
+									'date' => time(),
+									));
+							}
+							catch (Exception $e)
+							{
+									return $e->getMessage();
+							}
+					}
+				}
+			}
+		}
+		return $error;
+	}
+}
--- pages/inscription.php
+++ pages/inscription.php
@@ -1,14 +1,96 @@
 <?php
-echo <<<EOF
-<div class="style1">
-	<h3>Inscription</h3>
-	
+include('includes/inscription.inc.php');
+if(!isset($_POST['nom']) && !isset($_POST['prenom']) && !isset($_POST['status']) && !isset($_POST['email']) && !isset($_POST['mdp']))
+{
+
+echo '<div class="style1">
+	<h3>Incription</h3>
+	<div class="style1_contenu"></div>
+	</div> <!-- fin .style1_contenu -->
+</div> <!-- fin .style1 -->
+
 <div class="style2">
-	<h3>Sous-Titre</h3>
+	<h3>Informations</h3>
 	<div class="style2_contenu">
-	Ei possit nemore efficiantur eam, ei vix natum malis utamur. Sed habeo clita disputationi an. Mollis virtute quo cu, qui molestiae scripserit ea. Tritani ceteros ea est, ne etiam quaeque recusabo eam. Puto feugiat ex mea,
-	laudem putent argumentum has ex, mea an volutpat ocurreret. Solum invidunt referrentur his ei, ad simul voluptatibus nam.</div>
+	<form action="'.$ROOT.'/index.php?page=inscrip" method="post">
+	<table border="0">
+		<tr>
+			<td>Nom</td>
+			<td><input type="text" name="nom" id="nom"/></td>
+		</tr>
+		<tr>
+			<td>Prénom</td>
+			<td><input type="text" name="prenom" id="prenom" /></td>
+		</tr>
+		<tr>
+			<td>Status</td>
+			<td>';
+			try
+			{
+				$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+				$bdd = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_DBNAME, $DB_USERNAME, $DB_PASSWORD, $pdo_options);
+				$reponse = $bdd->query('SELECT * FROM status');
+				echo '<select name="status">';
+				while ($donnees = $reponse->fetch())
+				{
+					echo '<OPTION VALUE="'. $donnees['ID_STATUS'] . '">' . $donnees['NOM'] . '</OPTION>';
+				}
+				$reponse->closeCursor();
+			}
+			catch(Exception $e)
+			{
+				die('Erreur : '.$e->getMessage());
+			}
+			echo'</select>
+			</td>
+		</tr>
+	</table>
+	<br />
 	</div><!-- fin .style2_contenu -->
-</div> <!-- fin .style2 -->
-EOF;
+	<h3>Informations de connexion</h3>	
+	<div class="style2_contenu">	
+	<table>
+		<tr>
+			<td>Email</td>
+			<td><input type="text" name="email" id="email" /></td>
+		</tr>
+		<tr>
+			<td>Mot de Passe</td>
+			<td><input type="password" name="mdp" /></td>
+		</tr>
+		<tr>
+	</table>
+	<br />
+		<input type="submit" value="Valider" />
+	</div>
+	</div><!-- fin .style2_contenu -->
+</div> <!-- fin .style2 -->';
+}
+else
+{
+	$inscription = new Inscription();
+	$resultat = $inscription->register_values($_POST['nom'],$_POST['prenom'],$_POST['status'],$_POST['email'],$_POST['mdp']);
+	if($resultat!="noerror")
+	{
+		echo '
+		<div class="style1">
+			<h3>Inscription</h3>
+			<div class="style1_contenu"></div>
+				Une erreur est survenue lors de votre inscription : <br /> <h4>' . $resultat . '</h4>
+			</div> <!-- fin .style1_contenu -->
+		</div> <!-- fin .style1 -->	
+		</div>';
+	}
+	else
+	{
+	echo '
+		<div class="style1">
+			<h3>Inscription</h3>
+			<div class="style1_contenu"></div>
+				<h4>L\'inscription c\'est bien déroulée vous pouvez maintenant vous connecter</h4>
+			</div> <!-- fin .style1_contenu -->
+		</div> <!-- fin .style1 -->	
+		</div>';
+	}
+}
 ?>
